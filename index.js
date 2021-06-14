5de18a68a9a0679ab1a1380f4ee9aefa4cb776f5
SB17BLU <75648758+SB17BLU@users.noreply.github.com>
2021-06-14 16:02:17 -0400

    edit 1

---
+++ index.js
@@ -0,0 +1,6 @@
+function search_animal() {
+  var search = document.getElementById("animal_textbox").value;
+  if (search == "Koala"){
+    document.getElementById("animal_image").src = "Koala.jpg"
+  }
+}
